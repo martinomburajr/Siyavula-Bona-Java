26a3b63b0706aff9cb63dd6c4ba775d18877f898
martin-omburajr <martin.omburajr@gmail.com>
2015-08-13 09:33:46 +0200

    Created separate Parser Class that will do all the parsing and adding to the graph

--- src/com/bona/Driver/Driver.java
+++ src/com/bona/Driver/Driver.java
@@ -10,107 +10,15 @@ import org.jgrapht.DirectedGraph;
 import com.bona.Entity.Edge;
 import com.bona.Entity.Graph;
 import com.bona.Entity.Group;
+import com.bona.Entity.Parser;
 import com.bona.Entity.Vertex;
 import com.bona.Entity.Cycle.TarjanCycle;
 import com.bona.Entity.ShortestPathAlgorithm.BellmanFord;
 
 public class Driver {
-	static ArrayList<Vertex> vertices = new ArrayList<Vertex>();
-	static ArrayList<Group> groups = new ArrayList<Group>();
+
 	final static String FILEPATH = "00-chem_matter_source.map";
-	
-	public static void parseFile(String textFile) throws IOException
-	{
-		 //textFile = "00-chem_matter_source.map";
-		 BufferedReader in = new BufferedReader(new FileReader(FILEPATH));		 
-		 String x;
-		 boolean exists = false;
-		 boolean adding = true;
-		 
-		 String label = null;
-		 String type = "default";
-		 String id = null;
-		 Vertex n;
-		 Group g;
-		 
-        while (true)
-        {
-       	 x= in.readLine();
-       	// System.out.print(x + "\n");
-       	 if (x == null )
-				break;
-       	 if (x.contains("CMA") && adding){	
-       		 id=getID(x);
-       		 
-       		 for (Vertex o: vertices){
-       			 if (id.equals(o.getId()))
-       				 exists = true;	 
-       		 }
-       		 
-       		 if (exists==false){
-       			 System.out.println(id);
-       			 if (x.contains("type"))
-       				 type = x.substring(x.indexOf("type") + 6, x.indexOf(",")-1);
-       			 System.out.println(type);
-       			 if (x.contains("label"))
-       				 label = x.substring(x.indexOf("label") + 7, x.indexOf("]")-1);
-       			 System.out.println(label + "\n");
-       			 n = new Vertex(id, type,label);
-   	        	 vertices.add(n);
-   	        	 
-   	        	 if (type.equals("group")){
-   	        		 g = new Group(id,label);
-   	        		 groups.add(g);
-   	        	 }
-           	 } 
-       	 }
-       	 else if (x.contains("GROUPINGS")){
-       		 adding=false;
-       	 }
-       	 else if (x.contains("{")){
-       		 makeGroup(x,in);
-       		 
-       	 }
-       	 
-        }
-	 in.close();
-	 }
-	 
-	private static void makeGroup(String x, BufferedReader in) throws IOException {
-		
-		 String id = getID(x);
-		 for (Group group: groups){
-			 if (id.equals(group.getId())){
-				 while (true){
-   				 x= in.readLine();
-   				 if (x.contains("{"))
-   					 makeGroup(x,in);
-   				 else if (x.contains("CMA")){
-       				 id=getID(x);
-       				 for (Vertex node: vertices){
-               			 if (id.equals(node.getId())){
-               				 group.AddToGroup(node);	
-               			 	System.out.println(node.getId() + " is in the group " + group.getLabel());
-               			 }
-               		 }
-   				 }
-   				 else if(x.contains("}")){
-       				 System.out.println(x);
-   					 break;
-   				 }
-				 }
-			 }	 	 
-		 }
-	}
-	
-	public static String getID(String x){
-		 String id = x.substring(x.indexOf("CMA"));
-		 if (id.contains(" ")){
-			 id = ( id.substring(0, id.indexOf(" ")));
-		 }
-		return id;	 
-	 }
-	
+
 	/*
 	 * The bellmanFord shortest path algorithm will locate the shortest paths between nodes
 	 */
@@ -123,8 +31,7 @@ public class Driver {
 				shortestPath = bf.getPathEdgeList(graph.getVertices().get(17));
 				//shortestPath = BellmanFord.findPathBetween(graph, v1, v4);
 		return shortestPath;
-	}
-	
+	}	
 	/*
 	 * The TarjanCycle Class is responsible for identifying simple cycles within the graph
 	 */
@@ -227,13 +134,11 @@ public class Driver {
 	}
 	
 	public static void main(String args[]) throws IOException{
-		//Driver.parseFile(FILEPATH);
-		for(List<Vertex> lV : Driver.tarjanCycleTest())
-		{
-			for(Vertex v : lV){
-				System.out.println(v);
-			}			
-		}
+		Parser.parseFile(FILEPATH);
+		
+		System.out.println("Groups Size: " + Parser.getGroups().size());
+		System.out.println("Vertices Size: " + Parser.getVertices().size());
+		
 	}
 
 
---
+++ src/com/bona/Entity/Parser.java
@@ -0,0 +1,165 @@
+package com.bona.Entity;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.jgrapht.DirectedGraph;
+
+import com.bona.Entity.Group;
+import com.bona.Entity.Vertex;
+
+public class Parser implements Runnable {
+	private static ArrayList<Vertex> vertices = new ArrayList<Vertex>();
+	private static ArrayList<Group> groups = new ArrayList<Group>();
+	private static ArrayList<Edge> edges = new ArrayList<Edge>();
+	
+	public static void parseFile(String textFile) throws IOException
+	{
+		 //textFile = "00-chem_matter_source.map";
+		 BufferedReader in = new BufferedReader(new FileReader(textFile));		 
+		 String x;
+		 boolean exists = false;
+		 boolean adding = true;
+		 
+		 String label = null;
+		 String type = "default";
+		 String id = null;
+		 Vertex n;
+		 Group g;
+		 
+        while (true)
+        {
+       	 x= in.readLine();
+       	// System.out.print(x + "\n");
+       	 if (x == null )
+				break;
+       	 if (x.contains("CMA") && adding){	
+       		 id=getID(x);
+       		 
+       		 for (Vertex o: vertices){
+       			 if (id.equals(o.getId()))
+       				 exists = true;	 
+       		 }
+       		 
+       		 if (exists==false){
+       			 System.out.println(id);
+       			 if (x.contains("type"))
+       				 type = x.substring(x.indexOf("type") + 6, x.indexOf(",")-1);
+       			 System.out.println(type);
+       			 if (x.contains("label"))
+       				 label = x.substring(x.indexOf("label") + 7, x.indexOf("]")-1);
+       			 System.out.println(label + "\n");
+       			 n = new Vertex(id, type,label);
+   	        	 vertices.add(n);
+   	        	 
+   	        	 if (type.equals("group")){
+   	        		 g = new Group(id,label);
+   	        		 groups.add(g);
+   	        	 }
+           	 } 
+       	 }
+       	 else if (x.contains("GROUPINGS")){
+       		 adding=false;
+       	 }
+       	 else if (x.contains("{")){
+       		 makeGroup(x,in);
+       		 
+       	 }
+       	 
+        }
+	 in.close();
+	 }
+	 
+	private static void makeGroup(String x, BufferedReader in) throws IOException {
+		
+		 String id = getID(x);
+		 for (Group group: groups){
+			 if (id.equals(group.getId())){
+				 while (true){
+   				 x= in.readLine();
+   				 if (x.contains("{"))
+   					 makeGroup(x,in);
+   				 else if (x.contains("CMA")){
+       				 id=getID(x);
+       				 for (Vertex node: vertices){
+               			 if (id.equals(node.getId())){
+               				 group.AddToGroup(node);	
+               			 	System.out.println(node.getId() + " is in the group " + group.getLabel());
+               			 }
+               		 }
+   				 }
+   				 else if(x.contains("}")){
+       				 System.out.println(x);
+   					 break;
+   				 }
+				 }
+			 }	 	 
+		 }
+	}
+	
+	private static DirectedGraph<Vertex,Edge> addVerticesToGraph()
+	{
+		DirectedGraph<Vertex,Edge> graph = (DirectedGraph<Vertex,Edge>) new Graph();
+		
+		for(int i = 0; i < Parser.getVertices().size(); i++)
+		{
+			graph.addVertex(Parser.getVertices().get(i));
+		}
+		
+		for(int i = 0; i < Parser.getEdges().size(); i++)
+		{
+			//graph.addEdge(sourceVertex, targetVertex)
+		}
+		
+		return graph;
+	}
+	
+	public static String getID(String x){
+		 String id = x.substring(x.indexOf("CMA"));
+		 if (id.contains(" ")){
+			 id = ( id.substring(0, id.indexOf(" ")));
+		 }
+		return id;	 
+	 }
+
+	
+	public static ArrayList<Vertex> getVertices() {
+		return vertices;
+	}
+
+	public static void setVertices(ArrayList<Vertex> vertices) {
+		Parser.vertices = vertices;
+	}
+
+	public static ArrayList<Group> getGroups() {
+		return groups;
+	}
+
+	public static void setGroups(ArrayList<Group> groups) {
+		Parser.groups = groups;
+	}
+
+	
+	public static ArrayList<Edge> getEdges() {
+		return edges;
+	}
+
+	public static void setEdges(ArrayList<Edge> edges) {
+		Parser.edges = edges;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+
+
+}
+	 	
+
