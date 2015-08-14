6e4080d67f3cf128be4ed8765f915fb8ba1ea2fa
martin-omburajr <martin.omburajr@gmail.com>
2015-08-14 11:41:13 +0200

    just added a lil to the graphcontroller for it to persist information to the view. The graph controller will retrieve the graph created by the Parser class and give the view access to display it

--- src/com/bona/Controller/GraphController.java
+++ src/com/bona/Controller/GraphController.java
@@ -1,5 +1,20 @@
 package com.bona.Controller;
 
+import org.jgrapht.DirectedGraph;
+
+import com.bona.Entity.Edge;
+import com.bona.Entity.Graph;
+import com.bona.Entity.Parser;
+import com.bona.Entity.Vertex;
+
 public class GraphController {
 
+	public DirectedGraph<Vertex, Edge> persistGraph()
+	{
+		Graph graph = new Graph();
+		
+		//graph = Parser.
+		
+		return graph;
+	}
 }
