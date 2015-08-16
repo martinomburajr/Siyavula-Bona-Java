6c50bb9a302aa67d35494c422fd5f7ef31662cff
martin-omburajr <martin.omburajr@gmail.com>
2015-08-16 18:28:50 +0200

    Dabbled in some UI stuff, there is an error I think!

--- src/com/bona/View/UIDriver.java
+++ src/com/bona/View/UIDriver.java
@@ -1,10 +1,117 @@
 package com.bona.View;
 
-public class UIDriver {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.geom.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import javax.swing.JApplet;
+import javax.swing.JFrame;
 
-	}
+import org.jgraph.JGraph;
+import org.jgraph.graph.AttributeMap;
+import org.jgraph.graph.DefaultGraphCell;
+import org.jgraph.graph.GraphConstants;
+import org.jgrapht.ListenableGraph;
+import org.jgrapht.ext.JGraphModelAdapter;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.ListenableDirectedGraph;
 
+import com.bona.Entity.Edge;
+import com.bona.Entity.Vertex;
+
+
+
+public class UIDriver extends JApplet {
+    private static final Color     DEFAULT_BG_COLOR = Color.decode( "#FAFBFF" );
+    private static final Dimension DEFAULT_SIZE = new Dimension( 530, 320 );
+
+    // 
+    private JGraphModelAdapter m_jgAdapter;
+
+    /**
+     * @see java.applet.Applet#init().
+     */
+    public void init(  ) {
+        // create a JGraphT graph
+        ListenableGraph g = new ListenableDirectedGraph( DefaultEdge.class );
+
+        // create a visualization using JGraph, via an adapter
+        jgAdapter = new JGraphModelAdapter( g );
+
+        JGraph jgraph = new JGraph( jgAdapter );
+
+        adjustDisplaySettings( jgraph );
+        getContentPane(  ).add( jgraph );
+        resize( DEFAULT_SIZE );
+
+        // add some sample data (graph manipulated via JGraphT)
+        g.addVertex( "v1" );
+        g.addVertex( "v2" );
+        g.addVertex( "v3" );
+        g.addVertex( "v4" );
+
+        g.addEdge( "v1", "v2" );
+        g.addEdge( "v2", "v3" );
+        g.addEdge( "v3", "v1" );
+        g.addEdge( "v4", "v3" );
+
+        // position vertices nicely within JGraph component
+        positionVertexAt( "v1", 130, 40 );
+        positionVertexAt( "v2", 60, 200 );
+        positionVertexAt( "v3", 310, 230 );
+        positionVertexAt( "v4", 380, 70 );
+
+        // that's all there is to it!...
+    }
+
+
+    private void adjustDisplaySettings( JGraph jg ) {
+        jg.setPreferredSize( DEFAULT_SIZE );
+
+        Color  c        = DEFAULT_BG_COLOR;
+        String colorStr = null;
+
+        try {
+            colorStr = getParameter( "bgcolor" );
+        }
+         catch( Exception e ) {}
+
+        if( colorStr != null ) {
+            c = Color.decode( colorStr );
+        }
+
+        jg.setBackground( c );
+    }
+
+    private JGraphModelAdapter<Vertex, Edge> jgAdapter = new JGraphModelAdapter<Vertex, Edge>();
+    
+    private void positionVertexAt( Object vertex, int x, int y ) {
+        DefaultGraphCell cell = jgAdapter.getVertexCell(vertex);
+        AttributeMap attr = cell.getAttributes();
+        java.awt.geom.Rectangle2D bounds = GraphConstants.getBounds(attr);
+
+        Rectangle2D newBounds = new Rectangle2D.Double(x,y,bounds.getWidth(),bounds.getHeight());
+
+        GraphConstants.setBounds(attr, newBounds);
+
+        // TODO: Clean up generics once JGraph goes generic
+        org.jgraph.graph.AttributeMap cellAttr = new AttributeMap();
+        cellAttr.put(cell, attr);
+        jgAdapter.edit(cellAttr, null, null, null);
+    }
+    
+    public static void main(String [] args)
+    {
+    	UIDriver ui = new UIDriver();
+    	ui.init();
+    	
+        JFrame frame = new JFrame();
+        frame.getContentPane().add(ui);
+        frame.setTitle("JGraphT Adapter to JGraph Demo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    	
+    }
+    
 }
